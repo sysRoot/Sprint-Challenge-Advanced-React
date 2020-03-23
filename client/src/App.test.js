eeb98a455c48fbc69524a9d71cd67ea98bf53889
Jason Cook <the.root.user@gmail.com>
2020-03-22 22:55:52 -0400

    test

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { render } from '@testing-library/react';
 import App from './App';
+import Player from './Components/Player.js'
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+test('it renders', ()=>{
+  render(<App />);
+})
+
